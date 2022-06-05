22f2aea17ad24cdaa465d61115dfb200d14e9efe
nealwaga <waganeal@gmail.com>
2022-06-05 19:45:37 +0300

    connecting to postgres

--- insta_project/settings.py
+++ insta_project/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'insta_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tribune',
+        'USER': 'neal',
+        'PASSWORD':'Wneal9.',
     }
 }
 
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
