79555823c4ba9c7667841c6c588f69863bea9bc0
nealwaga <waganeal@gmail.com>
2022-06-06 03:19:22 +0300

    updated settings file

--- insta_project/settings.py
+++ insta_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
     'auth',
     'post',
     'commment',
+    'bootstrap4',
 ]
 
 #Cloudinary Classes
@@ -136,6 +138,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
