f9572a53221ebee31b26d4b8a57653b6a1397ffd
nealwaga <waganeal@gmail.com>
2022-06-06 05:12:25 +0300

    fixed bug

--- insta/admin.py
+++ insta/admin.py
@@ -1,5 +1,5 @@
-from django.contrib import admin
-from .models import *
+# from django.contrib import admin
+# from .models import *
 
-# Register your models here.
-admin.site.register(Profile)
+# # Register your models here.
+# admin.site.register(Profile)
