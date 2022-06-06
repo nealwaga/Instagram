77f787b77b1ee58ac2e49b812d5ead3127c5dd9e
nealwaga <waganeal@gmail.com>
2022-06-06 03:21:06 +0300

    updated settings file

--- insta_project/settings.py
+++ insta_project/settings.py
@@ -142,6 +142,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
