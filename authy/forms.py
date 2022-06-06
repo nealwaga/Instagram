38bfe66d9ed683e64b39273130d42f4c665ee5ba
nealwaga <waganeal@gmail.com>
2022-06-06 04:51:46 +0300

    updated comments

--- authy/forms.py
+++ authy/forms.py
@@ -2,10 +2,11 @@ from django import forms
 from authy.models import Profile
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from cloudinary.models import CloudinaryField
 
 # Create forms here
 class EditProfileForm(forms.ModelForm):
-    image = forms.ImageField(required=True)
+    image = CloudinaryField(required=True)
     first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'First Name'}), required=True)
     last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Last Name'}), required=True)
     bio = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Bio'}), required=True)
@@ -21,7 +22,6 @@ class EditProfileForm(forms.ModelForm):
 class UserRegisterForm(UserCreationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'prompt srch_explore'}), max_length=50, required=True)
     # username = forms.EmailInput(widget=forms.TextInput(attrs={'placeholder': 'Username'}), max_length=50, required=True)
-
     email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email', 'class': 'prompt srch_explore'}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter Password', 'class': 'prompt srch_explore'}))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password', 'class': 'prompt srch_explore'}))
