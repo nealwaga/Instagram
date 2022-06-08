6c0f3fac5137bc78d4596b2388181a093a33eeee
nealwaga <waganeal@gmail.com>
2022-06-08 22:17:07 +0300

    django form errors

--- authy/views.py
+++ authy/views.py
@@ -106,11 +106,13 @@ def register(request):
             new_user = form.save()
             # Profile.get_or_create(user=request.user)
             username = form.cleaned_data.get('username')
+            print(username)
             messages.success(request, f'Hurray your account was created!!')
 
             # Automatically Log In The User
             new_user = authenticate(username=form.cleaned_data['username'],
                                     password=form.cleaned_data['password1'],)
+            print(new_user)
             login(request, new_user)
             # return redirect('editprofile')
             return redirect('index')
